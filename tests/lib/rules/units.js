35f29fa6aa4aab678d48376c2778411a78e73234
Divyagnan <divyagnan@users.noreply.github.com>
2017-06-28 18:41:21 -0400

    better detection of when to check

--- tests/lib/rules/units.js
+++ tests/lib/rules/units.js
@@ -44,6 +44,17 @@ ruleTester.run("units", rule, {
         class Component extends React.Component {
           render() {
             <div>
+              <span style={{ height: "40px" }} color="red">stuff</span>
+            </div>;
+          }
+        }
+      `
+    },
+    {
+      code: `
+        class Component extends React.Component {
+          render() {
+            <div>
               <App props={{ someProp: 40 }} color="red">stuff</App>
             </div>;
           }
@@ -56,6 +67,160 @@ ruleTester.run("units", rule, {
           render() {
             <div>
               <span style={{ color: 'red' }} color="red">stuff</span>
+            </div>;
+          }
+        }
+      `
+    },
+    {
+      code: `
+        class Component extends React.Component {
+          render() {
+            <div>
+              <span style={{ color: "red" }} color="red">stuff</span>
+            </div>;
+          }
+        }
+      `
+    },
+    {
+      code: `
+        class Component extends React.Component {
+          render() {
+            <div>
+              <span style={{ height: "40%" }} color="red">stuff</span>
+            </div>;
+          }
+        }
+      `
+    },
+    {
+      code: `
+        class Component extends React.Component {
+          render() {
+            <div>
+              <span style={{ height: "40pt" }} color="red">stuff</span>
+            </div>;
+          }
+        }
+      `
+    },
+    {
+      code: `
+        class Component extends React.Component {
+          render() {
+            <div>
+              <span style={{ height: "40vh" }} color="red">stuff</span>
+            </div>;
+          }
+        }
+      `
+    },
+    {
+      code: `
+        class Component extends React.Component {
+          render() {
+            <div>
+              <span style={{ height: "40vw" }} color="red">stuff</span>
+            </div>;
+          }
+        }
+      `
+    },
+    {
+      code: `
+        class Component extends React.Component {
+          render() {
+            <div>
+              <span style={{ height: "40pt" }} color="red">stuff</span>
+            </div>;
+          }
+        }
+      `
+    },
+    {
+      code: `
+        class Component extends React.Component {
+          render() {
+            <div>
+              <span style={{ height: "40pc" }} color="red">stuff</span>
+            </div>;
+          }
+        }
+      `
+    },
+    {
+      code: `
+        class Component extends React.Component {
+          render() {
+            <div>
+              <span style={{ height: "40vmin" }} color="red">stuff</span>
+            </div>;
+          }
+        }
+      `
+    },
+    {
+      code: `
+        class Component extends React.Component {
+          render() {
+            <div>
+              <span style={{ height: "40vmax" }} color="red">stuff</span>
+            </div>;
+          }
+        }
+      `
+    },
+    {
+      code: `
+        class Component extends React.Component {
+          render() {
+            <div>
+              <span style={{ height: "40ch" }} color="red">stuff</span>
+            </div>;
+          }
+        }
+      `
+    },
+    {
+      code: `
+        class Component extends React.Component {
+          render() {
+            <div>
+              <span style={{ height: "40cm" }} color="red">stuff</span>
+            </div>;
+          }
+        }
+      `
+    },
+    {
+      code: `
+        class Component extends React.Component {
+          render() {
+            <div>
+              <span style={{ height: "40mm" }} color="red">stuff</span>
+            </div>;
+          }
+        }
+      `
+    },
+    {
+      code: `
+        class Component extends React.Component {
+          render() {
+            <div>
+              <span style={{ height: "40ex" }} color="red">stuff</span>
+            </div>;
+          }
+        }
+      `
+    },
+    {
+      code: `
+        class Component extends React.Component {
+          render() {
+            <div>
+              <span style={{ height: "40in" }} color="red">stuff</span>
             </div>;
           }
         }
