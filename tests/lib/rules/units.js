45377d4889c5c0cdec5acbf6701295852e55624e
Divyagnan <divyagnan@users.noreply.github.com>
2017-06-28 18:13:44 -0400

    feat: inital prop checks

--- tests/lib/rules/units.js
+++ tests/lib/rules/units.js
@@ -8,30 +8,180 @@
 // Requirements
 //------------------------------------------------------------------------------
 
-var rule = require("../../../lib/rules/units"),
+const rule = require("../../../lib/rules/units");
+const RuleTester = require("eslint").RuleTester;
 
-    RuleTester = require("eslint").RuleTester;
+require("babel-eslint");
 
+const parserOptions = {
+  ecmaVersion: 8,
+  sourceType: "module",
+  ecmaFeatures: {
+    jsx: true
+  }
+};
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
-var ruleTester = new RuleTester();
+var ruleTester = new RuleTester({ parserOptions });
 ruleTester.run("units", rule, {
-
-    valid: [
-
-        // give me some code that won't trigger a warning
-    ],
-
-    invalid: [
-        {
-            code: "<div style={{height: 40}}>hi</div>",
-            errors: [{
-                message: "Fill me in.",
-                type: "Me too"
-            }]
+  valid: [
+    {
+      code: `
+        class Component extends React.Component {
+          render() {
+            <div>
+              <span style={{ height: '40px' }} color="red">stuff</span>
+            </div>;
+          }
         }
-    ]
+      `
+    },
+    {
+      code: `
+        class Component extends React.Component {
+          render() {
+            <div>
+              <App props={{ someProp: 40 }} color="red">stuff</App>
+            </div>;
+          }
+        }
+      `
+    },
+    {
+      code: `
+        class Component extends React.Component {
+          render() {
+            <div>
+              <span style={{ color: 'red' }} color="red">stuff</span>
+            </div>;
+          }
+        }
+      `
+    }
+  ],
+
+  invalid: [
+    {
+      code: `
+        class Component extends React.Component {
+          render() {
+            <div>
+              <span style={{ height: 40 }} color="red">stuff</span>
+            </div>;
+          }
+        }
+      `,
+      errors: [
+        {
+          message: "Expected an explicit unit",
+          type: "JSXOpeningElement"
+        }
+      ]
+    },
+    {
+      code: `
+        class Component extends React.Component {
+          render() {
+            <div>
+              <span style={{ margin: 40 }} color="red">stuff</span>
+            </div>;
+          }
+        }
+      `,
+      errors: [
+        {
+          message: "Expected an explicit unit",
+          type: "JSXOpeningElement"
+        }
+      ]
+    },
+    {
+      code: `
+        class Component extends React.Component {
+          render() {
+            <div>
+              <span style={{ padding: 40 }} color="red">stuff</span>
+            </div>;
+          }
+        }
+      `,
+      errors: [
+        {
+          message: "Expected an explicit unit",
+          type: "JSXOpeningElement"
+        }
+      ]
+    },
+    {
+      code: `
+        class Component extends React.Component {
+          render() {
+            <div>
+              <span style={{ marginRight: 40 }} color="red">stuff</span>
+            </div>;
+          }
+        }
+      `,
+      errors: [
+        {
+          message: "Expected an explicit unit",
+          type: "JSXOpeningElement"
+        }
+      ]
+    },
+    {
+      code: `
+        class Component extends React.Component {
+          render() {
+            <div>
+              <span style={{ paddingLeft: 40 }} color="red">stuff</span>
+            </div>;
+          }
+        }
+      `,
+      errors: [
+        {
+          message: "Expected an explicit unit",
+          type: "JSXOpeningElement"
+        }
+      ]
+    },
+    {
+      code: `
+        class Component extends React.Component {
+          render() {
+            <div>
+              <span style={{ left: 40 }} color="red">stuff</span>
+            </div>;
+          }
+        }
+      `,
+      errors: [
+        {
+          message: "Expected an explicit unit",
+          type: "JSXOpeningElement"
+        }
+      ]
+    },
+    {
+      code: `
+        class Component extends React.Component {
+          render() {
+            <div>
+              <span style={{ right: 40 }} color="red">stuff</span>
+            </div>;
+          }
+        }
+      `,
+      errors: [
+        {
+          message: "Expected an explicit unit",
+          type: "JSXOpeningElement"
+        }
+      ]
+    }
+  ]
 });
